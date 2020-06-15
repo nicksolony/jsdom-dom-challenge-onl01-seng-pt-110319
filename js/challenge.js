4c675373acff1cc7ad03cdd32ae6d397a1a31d4c
nicksolony <nicksolony@gmail.com>
2020-06-14 22:28:51 -0400

    added likes

--- js/challenge.js
+++ js/challenge.js
@@ -1,8 +1,43 @@
 let counter = document.querySelector("#counter");
+let plus = document.querySelector("#plus");
+let minus = document.querySelector("#minus");
+let likes = document.querySelector("ul.likes");
+let like = document.querySelector("#heart");
+let likeCount=0;
+let runTimer = setInterval(updateCounter,1000);
 
-let runTimer = setInterval(update_counter,1000);
-
-
-function update_counter(){
+function updateCounter(){
   counter.innerText=parseInt(counter.innerText)+1;
-}
+};
+
+function decreaseCounter(){
+  counter.innerText=parseInt(counter.innerText)-1;
+};
+
+function addLike() {
+  if (likes.lastChild) {
+    let li = likes.lastChild;
+    if (li.id===counter.innerText) {
+      likeCount++;
+      li.innerText=`${li.id} has been liked ${likeCount} times`;
+    } else {
+      likeCount=1;
+      let li = document.createElement("li");
+      li.id=parseInt(counter.innerText);
+      li.innerText=`${li.id} has been liked 1 time`;
+      likes.appendChild(li);
+    }
+
+  } else {
+    likeCount=0;
+    let li = document.createElement("li");
+    li.id=parseInt(counter.innerText);
+    likeCount++;
+    li.innerText=`${parseInt(counter.innerText)} has been liked 1 time`;
+    likes.appendChild(li);
+  }
+};
+
+plus.addEventListener("click", updateCounter);
+minus.addEventListener("click", decreaseCounter);
+like.addEventListener("click",addLike);
